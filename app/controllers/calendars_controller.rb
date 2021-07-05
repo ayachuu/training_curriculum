d0853f276f7d0e670d71f105c18ead23a1de68fa
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-05 21:22:29 +0900

    ブランチ切り替えミスによる再作業

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,7 +20,7 @@ class CalendarsController < ApplicationController
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    #3 wdays[number]
+
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -35,13 +35,13 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x #1
+      # ブランチの切り替えミスによりコントローラーに差分が出ないためこのコメントを記述
+      wday_num = Date.today.wday + x 
       if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { month： (@todays_date + x).month, date： (@todays_date+x).day, 　plans：　today_plans, wday：　wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
 
       @week_days.push(days)
     end
