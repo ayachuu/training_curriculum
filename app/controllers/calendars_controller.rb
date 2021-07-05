1139afd11585d0da8fe77ab2e7a575eefe41f61f
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-05 14:40:45 +0900

    Revert "Issue4"
    This reverts commit 3b00ac6e32c63e7ea3771fa1ca7341ffa498a485.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:plan, :date)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def getWeek
